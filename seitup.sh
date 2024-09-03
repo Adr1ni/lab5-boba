3c0dbb092cc6bca6f81e462279059bf02bf5239e
AdrianoGongora <adriano.gongora.juarez@gmail.com>
2024-09-03 11:02:06 -0500

    add: automatiation

---
+++ seitup.sh
@@ -0,0 +1,22 @@
+clone_repository() {
+  local repo_url="https://github.com/Adr1ni/lab5-boba"
+  if [ ! -d "exam-boba" ]; then
+    echo "Cloning the repository..."
+    git clone $repo_url
+  else
+    echo "Repository already cloned."
+  fi
+}
+
+build_and_run_container() {
+  cd lab5-boba
+
+  echo "Running Docker..."
+  docker build -t hola-mundo-multiple .
+
+  echo "Running Docker..."
+  docker run --rm hola-mundo-multiple
+}
+
+clone_repository
+build_and_run_container
